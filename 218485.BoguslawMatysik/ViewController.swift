bd7a9f52e18ed113b825e928d087f2df3ef46b48
Boguslaw Matysik <218485@student.pwr.edu.pl>
2020-05-16 01:28:09 +0200

    first working version with rate slider

--- 218485.BoguslawMatysik/ViewController.swift
+++ 218485.BoguslawMatysik/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var mealPrice: UITextField!
+    
+    @IBOutlet weak var finalPrice: UILabel!
+    
+    private var mealPriceNumber : Float?{
+        get{
+            Float(mealPrice.text!)
+        }
+    }
+    
+    private var serviceRateValue: Float = 0.0
+    
+    @IBAction func serviceRate(_ sender: UISlider) {
+        serviceRateValue = sender.value
+    }
+    
+    
+    
+    @IBAction func calculatePrice(_ sender: UIButton) {
+        print(" was called")
+        if mealPriceNumber != nil {
+            finalPrice.text =  String(mealPriceNumber! * (serviceRateValue + 100) / 100)
+        }
 
+    }
 }
 
