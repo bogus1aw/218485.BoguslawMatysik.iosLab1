eb4fd39a2dc508973a7710b72f8f1ca75f1eebb2
Boguslaw Matysik <218485@student.pwr.edu.pl>
2020-05-15 17:20:02 +0200

    init single page project

---
+++ 218485.BoguslawMatysik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  218485.BoguslawMatysik
+//
+//  Created by Student on 15/05/2020.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
